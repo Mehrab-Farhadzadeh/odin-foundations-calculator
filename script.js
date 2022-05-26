82a6bcaf682289700345a1c0f3f81798c46301e7
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2022-05-27 00:30:30 +0430

    Refactoring Theme Palette code
    Rewrite some functions.
    Rename some variables and functions.

--- script.js
+++ script.js
@@ -1,52 +1,41 @@
 // ***********************
 // **** Theme Palette ****
 // ***********************
-function toggleAClassThroughElements(allElements, clsName, activatedElement) {
-  for (const element of allElements) {
-    if (element.className.includes(clsName)) element.classList.remove(clsName);
-  }
-  activatedElement.classList.add(clsName);
-}
+const ACTIVE_THEME_CLASS_NAME = "active";
+
 function getCurrentTheme() {
   return document.body.classList[0];
 }
-function activateTheme(newTheme) {
+function toggleActiveButton(newButton) {
+  const currentActiveButton = document.querySelector(
+    ".themePalette button.active"
+  );
+  currentActiveButton.classList.remove(ACTIVE_THEME_CLASS_NAME);
+  newButton.classList.add(ACTIVE_THEME_CLASS_NAME);
+}
+function applyTheme(newTheme) {
   const currentTheme = getCurrentTheme();
-  const elements = [document.body];
-  elements.push(document.querySelector(".calculator"));
-  elements.push(document.querySelector(".screen .history"));
-  elements.push(document.querySelector(".screen .result"));
-  elements.push(...document.querySelectorAll(".calculator button"));
-  elements.push(document.querySelector("footer"));
+  const elements = [
+    document.body,
+    document.querySelector(".calculator"),
+    document.querySelector(".screen .history"),
+    document.querySelector(".screen .result"),
+    ...document.querySelectorAll(".calculator button"),
+    document.querySelector("footer"),
+  ];
   for (const element of elements) {
     element.className = element.className.replace(currentTheme, newTheme);
   }
 }
-function changeTheme(event) {
-  toggleAClassThroughElements(
-    Array.from(document.querySelectorAll(".themePalette button")),
-    "active",
-    event.target
-  );
-  const newTheme = event.target.classList[0];
-  switch (newTheme) {
-    case "white":
-      activateTheme("white");
-      break;
-    case "glass":
-      activateTheme("glass");
-      break;
-    case "black":
-      activateTheme("black");
-      break;
-    default:
-      console.log(event.className);
-  }
+function applyChosenTheme() {
+  const newTheme = this.classList[0];
+  applyTheme(newTheme);
+  toggleActiveButton(this);
 }
 function activateThemePalette() {
   const themeButtons = document.querySelectorAll(".themePalette button");
   themeButtons.forEach((element) => {
-    element.addEventListener("click", changeTheme);
+    element.addEventListener("click", applyChosenTheme);
   });
 }
 
