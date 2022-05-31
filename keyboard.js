60c1ffad50e7db4ac251c64984bd9b7285c7b0ef
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2022-06-01 03:48:40 +0430

    Add and remove style when keydown and keyup

--- keyboard.js
+++ keyboard.js
@@ -27,34 +27,71 @@ const ID_S = {
    },
 };
 
+function addStyleToButton(id) {
+   const numButton = document.getElementById(id);
+   numButton.classList.add("pressed");
+}
+function removeStyleFromButton(id) {
+   const numButton = document.getElementById(id);
+   numButton.classList.remove("pressed");
+}
 function activateButtonKeyDown(event) {
    // * numbers *
    if (event.key in ID_S.numbers) {
-      const numButton = document.getElementById(ID_S.numbers[event.key]);
-      numButton.classList.add("pressed");
+      addStyleToButton(ID_S.numbers[event.key]);
       populateScreen(event.key);
    }
+
    // * operators *
    if (event.key in ID_S.operators) {
+      addStyleToButton(ID_S.operators[event.key]);
       handleOperatorButton(event.key);
    }
+
    // * equals *
-   if (ID_S[event.key] === "equals") equals();
+   if (ID_S[event.key] === "equals") {
+      addStyleToButton(ID_S[event.key]);
+      equals();
+   }
 
    // * dot *
-   if (ID_S[event.key] === "dot") dot();
+   if (ID_S[event.key] === "dot") {
+      addStyleToButton(ID_S[event.key]);
+      dot();
+   }
 
    // * backspace *
-   if (ID_S[event.key] === "backspace") backspace();
+   if (ID_S[event.key] === "backspace") {
+      addStyleToButton(ID_S[event.key]);
+      backspace();
+   }
 
    // * ac *
-   if (ID_S[event.key] === "ac") ac();
+   if (ID_S[event.key] === "ac") {
+      addStyleToButton(ID_S[event.key]);
+      ac();
+   }
 }
 function activateButtonKeyUp(event) {
-   if (event.key in ID_S.numbers) {
-      const numButton = document.getElementById(ID_S.numbers[event.key]);
-      numButton.classList.remove("pressed");
-   }
+   // * numbers *
+   if (event.key in ID_S.numbers)
+      removeStyleFromButton(ID_S.numbers[event.key]);
+
+   // * operators *
+   if (event.key in ID_S.operators)
+      removeStyleFromButton(ID_S.operators[event.key]);
+
+   // * equals *
+   if (ID_S[event.key] === "equals") removeStyleFromButton(ID_S[event.key]);
+
+   // * dot *
+   if (ID_S[event.key] === "dot") removeStyleFromButton(ID_S[event.key]);
+
+   // * backspace *
+   if (ID_S[event.key] === "backspace") removeStyleFromButton(ID_S[event.key]);
+
+   // * ac *
+   if (ID_S[event.key] === "ac") removeStyleFromButton(ID_S[event.key]);
 }
 function activateButtonsKeyboard() {
    window.addEventListener("keydown", activateButtonKeyDown);
